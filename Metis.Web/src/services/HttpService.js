b1178e729f12d102ef16d96de32797e3c45bad3f
akkgr <akkgr@users.noreply.github.com>
2019-04-23 13:02:51 +0300

    web env params

--- Metis.Web/src/services/HttpService.js
+++ Metis.Web/src/services/HttpService.js
@@ -1,25 +1,27 @@
-import { notification } from 'antd';
+import { notification } from 'antd'
 
-const api = 'http://localhost:58737/api/'
+const api = process.env.REACT_APP_API_URL + 'api/'
 export const callFetch = async (url, method, data) => {
-    const result = await (await fetch(api + url, {
-        method: method,
-        headers: { 'Content-Type': 'application/json' },
-        body: data ? JSON.stringify(data) : null
-    }).then(async res => {
-        if (res.ok) {
-            return res.json();
-        } else {
-            const message = await res.json();
-            notification['error']({
-                message: 'Σφάλμα',
-                description: message.Message
-            });
-            return null;
-        }
-    }).catch(() => {
-        // to do error handling
-    }));
+  const result = await await fetch(api + url, {
+    method: method,
+    headers: { 'Content-Type': 'application/json' },
+    body: data ? JSON.stringify(data) : null
+  })
+    .then(async res => {
+      if (res.ok) {
+        return res.json()
+      } else {
+        const message = await res.json()
+        notification['error']({
+          message: 'Σφάλμα',
+          description: message.Message
+        })
+        return null
+      }
+    })
+    .catch(() => {
+      // to do error handling
+    })
 
-    return result;
+  return result
 }
