7c5fb0840ad9349c1d0bcb64dc69c9b3076458a8
nperperidis <perpegr@hotmail.com>
2019-04-29 22:32:42 +0300

    login page

--- Metis.Web/src/components/login/Login.js
+++ Metis.Web/src/components/login/Login.js
@@ -1,12 +1,12 @@
 import React, { useState, useContext } from 'react';
 import { AuthContext } from '../../auth/AuthProvider';
-import { Form, Icon, Input, Button, Row, Col, Typography, Card } from 'antd';
+import { Form, Icon, Input, Button, Row, Col, Typography, Divider, Comment, Avatar, } from 'antd';
 import { callFetch } from '../../services/HttpService';
 import logo from '../../assets/badge-png-vector-5-transparent.png'
 import '../../styles/Utilities.sass';
 import './Login.sass';
 
-const Login = props => {
+const Login = () => {
     const authContext = useContext(AuthContext);
     const [loginModel, setLoginModel] = useState({
         username: '',
@@ -36,48 +36,59 @@ const Login = props => {
     }
     const clearButton = loginModel.username ? <Icon type="close-circle" onClick={clearUsernameHandler} /> : null;
     return (
-        <Row className="is-fullheight login-bck " type="flex" justify="space-around" align="middle">
-            <Col lg={{ span: 8 }} xl={{ span: 6 }} xxl={{ span: 4 }}>
-                <Card size="small" className="card-index">
-                    <Form >
-                        <Typography.Title className=" mb-5 has-text-dark" level={3}>
-                            <img className="logo-login" src={logo} alt="..." /> Σύνδεση
-                            </Typography.Title>
-                        <Form.Item className="m0 "
-                            label={<span className="has-text-dark">Όνομα Χρήστη</span>}
-                            {...formItemLayout}>
-                            <Input
-                                placeholder="Enter your username"
-                                name="username"
-                                autoComplete="username"
-                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                suffix={clearButton}
-                                className="has-text-dark"
-                                value={loginModel.username}
-                                onChange={inputsHandler} />
-                        </Form.Item>
-                        <Form.Item
-                            className="m0"
-                            label={<span className="has-text-dark">Κωδικός</span>}
-                            {...formItemLayout}>
-                            <Input.Password
-                                autoComplete="password"
-                                className="has-text-dark"
-                                name="password"
-                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                value={loginModel.password}
-                                onChange={inputsHandler}
-                                placeholder="Your password"
-                                onPressEnter={loginHandler} />
-                        </Form.Item>
-                        <Form.Item className="mb-5">
-                            <span className="" >Ξέχασα τον κωδικό μου</span>
-                        </Form.Item>
-                        <Form.Item>
-                            <Button type="primary" onClick={loginHandler} block> Σύνδεση</Button>
-                        </Form.Item>
-                    </Form>
-                </Card>
+        <Row className="is-fullheight" type="flex" justify="center" align="middle">
+            <Col xs={{ span: 0 }} md={{ span: 11 }} xl={{ span: 7 }} xxl={{ span: 5 }}>
+                <Comment
+                    author={<Typography.Title className="metis-title" level={4}>Metis</Typography.Title>}
+                    avatar={(
+                        <Avatar size={124}
+                            src={logo} alt="TODO" />
+                    )}
+                    content={(
+                        <p className="content-fix">Some text that they will propably provide for use about the application. Otherwise
+                        some basic info about what this application does.</p>
+                    )}
+                />
+            </Col>
+            <Col xs={{ span: 0 }} md={{ span: 1 }} className="has-text-centered">
+                <Divider type="vertical" className="divider" />
+            </Col>
+
+            <Col xs={{ span: 21 }} md={{ span: 11 }} xl={{ span: 7 }} xxl={{ span: 5 }}>
+
+                <Form >
+                    <Typography.Title className="mb-5" level={2}>
+                        Σύνδεση
+                    </Typography.Title>
+                    <Form.Item>
+                        <Input
+                            placeholder="Συμπληρώστε όνομα χρήστη"
+                            size="large"
+                            name="username"
+                            autoComplete="username"
+                            prefix={<Icon type="user" />}
+                            suffix={clearButton}
+                            className="has-text-dark"
+                            value={loginModel.username}
+                            onChange={inputsHandler} />
+                    </Form.Item>
+                    <Form.Item>
+                        <Input.Password
+                            autoComplete="password"
+                            size="large"
+                            className="has-text-dark"
+                            name="password"
+                            prefix={<Icon type="lock" />}
+                            value={loginModel.password}
+                            onChange={inputsHandler}
+                            placeholder="Ο κωδικός σας"
+                            onPressEnter={loginHandler} />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" size="large"
+                            onClick={loginHandler} block> Σύνδεση</Button>
+                    </Form.Item>
+                </Form>
             </Col>
         </Row>
     );
