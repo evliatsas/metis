8cb9bbae76bf1225282b6deda5e706b145b9404f
Antonis Katarachias <ak.hellas@gmail.com>
2019-05-24 10:08:34 +0300

    logbooks

--- Metis.Web/src/components/logBooks/LogBooks.js
+++ Metis.Web/src/components/logBooks/LogBooks.js
@@ -1,15 +1,90 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { Button as AntdButton, Tooltip as AntdTooltip } from 'antd'
+import moment from 'moment'
 import LogBooksContainer from '../containers/LogBooksContainer'
-import LogBooksHeader from './LogBooksHeader'
-import LogBooksTable from './LogBooksTable'
-import './logBooks.less'
+import PageHeader from '../shared/PageHeader'
+import Table from '../shared/Table'
+import LogBookStatus from './LogBookStatus'
+
+const STRINGS = {
+  TITLE: 'Βιβλία Καταγραφής',
+  SUBTITLE: 'Βιβλία καταγραφής συμβάντων',
+  REFRESH: 'Ανανέωση',
+  NEW: 'Νέο Βιβλίο',
+  EDIT: 'Επεξεργασία',
+  VIEW: 'Προβολή'
+}
+
+const columns = [
+  {
+    title: 'Τίτλος',
+    dataIndex: 'name',
+    key: 'name',
+    render: (e, row) => <NavLink to={`/logbooks/${row.id}`}>{row.name}</NavLink>
+  },
+  {
+    title: 'Λήξη',
+    dataIndex: 'close',
+    key: 'close',
+    render: item => moment(item).format('L')
+  },
+  {
+    title: 'Συντάκτης',
+    dataIndex: 'owner.name',
+    key: 'owner'
+  },
+  {
+    title: 'Γεγονότα',
+    dataIndex: 'entriesCount',
+    key: 'entriesCount'
+  },
+  {
+    title: 'Μέλη',
+    dataIndex: 'membersCount',
+    key: 'membersCount'
+  },
+  {
+    title: 'Κατάσταση',
+    dataIndex: 'status',
+    key: 'status',
+    render: (e, row) => <LogBookStatus closeDate={row.close} />
+  },
+  {
+    title: 'Ενέργειες',
+    key: 'action',
+    render: (e, row) => (
+      <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+        <AntdTooltip title={STRINGS.EDIT}>
+          <NavLink to={`/logbooks/${row.id}/edit`}>
+            <AntdButton size="small" type="ghost" shape="circle" icon="form" />
+          </NavLink>
+        </AntdTooltip>
+        <AntdTooltip title={STRINGS.VIEW}>
+          <NavLink to={`/logbooks/${row.id}`}>
+            <AntdButton size="small" type="ghost" shape="circle" icon="eye" />
+          </NavLink>
+        </AntdTooltip>
+      </div>
+    )
+  }
+]
 
 const LogBooksView = ({ logBooks, onCreate }) => {
   return (
     <div>
-      <LogBooksHeader onCreate={onCreate} />
+      <PageHeader
+        title={STRINGS.TITLE}
+        subtitle={STRINGS.SUBTITLE}
+        actions={[
+          {
+            caption: STRINGS.NEW,
+            onClick: onCreate
+          }
+        ]}
+      />
       <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <LogBooksTable logBooks={logBooks} />
+        <Table columns={columns} data={logBooks} />
       </div>
     </div>
   )
